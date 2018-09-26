bd6b4e3b70da242f5e60efeb757861868116bfbf
lofo <louisfournier@louiss-macbook.local>
2018-09-26 12:48:24 +0200

    Initial Commit

---
+++ SortedArray/SortedArray.h
@@ -0,0 +1,19 @@
+//
+//  SortedArray.h
+//  SortedArray
+//
+//  Created by Louis Fournier on 26/09/2018.
+//  Copyright © 2018 Louis Fournier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SortedArray.
+FOUNDATION_EXPORT double SortedArrayVersionNumber;
+
+//! Project version string for SortedArray.
+FOUNDATION_EXPORT const unsigned char SortedArrayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SortedArray/PublicHeader.h>
+
+
